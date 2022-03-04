b42a4fee23c5f99fd045b189f3190ae723f12a58
Japu431 <japamnk@gmail.com>
2022-03-04 11:26:51 -0300

    Classe Cliente Criado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using POO.classes.models;
+using POO.classes.controllers;
+
+namespace POO
+{
+    public class Program
+    {
+        Cliente cliente = new Cliente("1234",2000);
+    }
+}
---
+++ classes/controllers/AutenticacaoCliente.cs
@@ -0,0 +1,11 @@
+using POO.classes.models;
+
+namespace POO.classes.controllers
+{
+    public class AutenticacaoCliente : Cliente 
+    {
+        public AutenticacaoCliente(): base("123456" ,2000) {
+            
+        }
+    }
+}
